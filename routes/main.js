c06889625e6d4f9a8401391a7415cc005e08c9c5
matthewmonolith <matthew.page2016@outlook.com>
2023-09-10 11:18:44 +0100

    feed at a good level at this current time

--- routes/main.js
+++ routes/main.js
@@ -9,6 +9,8 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 router.get("/", homeController.getIndex);
 router.get("/profile", ensureAuth, postsController.getProfile);
 router.get("/feed", ensureAuth, postsController.getFeed);
+router.get("/feed/top", ensureAuth, postsController.getFeedTop);
+router.get("/feed/new", ensureAuth, postsController.getFeedNew);
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
 router.get("/logout", authController.logout);
